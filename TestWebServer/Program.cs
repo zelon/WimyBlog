4fe6f435c63138c0661214b6934d7db0eb004454
KIM, JINWOOK <zelonion@gmail.com>
2017-08-17 00:06:52 +0900

    Add TestWebServer project

---
+++ TestWebServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TestWebServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Need root directory path");
+                return;
+            }
+            string web_root_path = args[0];
+            Console.WriteLine("Web root path: {0}", web_root_path);
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot(web_root_path)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
