5603fe606a5a9427ff4e1732d7801846ae8300a6
KIM, JINWOOK <zelonion@gmail.com>
2017-08-16 22:36:58 +0900

    More flexible error handling

--- WimyBlog/PostCollector.cs
+++ WimyBlog/PostCollector.cs
@@ -18,15 +18,28 @@ namespace WimyBlog
         {
             List<Post> posts = new List<Post>();
             var directory_enumerator = Directory.EnumerateDirectories(config_.PostDirectory);
+            int error_count = 0;
             Parallel.ForEach(directory_enumerator, (string directory) =>
             {
-                var full_path = Path.GetFullPath(directory);
-                var name = Path.GetFileName(directory);
-                if (IsNumeric(name))
+                try
                 {
-                    posts.Add(Post.Convert(full_path, config_));
+                    var full_path = Path.GetFullPath(directory);
+                    var name = Path.GetFileName(directory);
+                    if (IsNumeric(name))
+                    {
+                        posts.Add(Post.Convert(full_path, config_));
+                    }
+                } catch (MetadataReader.MetadataReaderException exception)
+                {
+                    Console.WriteLine("Invalid metadata file. filename:{0}", exception.Message);
+                    ++error_count;
                 }
             });
+
+            if (error_count > 0)
+            {
+                Environment.Exit(1);
+            }
 
             posts.Sort((Post left, Post right) => right.Id.CompareTo(left.Id));
 
